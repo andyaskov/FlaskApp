89944c18d92a74acacbf619cdcc140bb880c2a5e
Andrey Yaskov <ayaskov93@gmail.com>
2018-02-05 11:08:36 +0300

    Implemented context manager

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,28 +1,28 @@
 from flask import Flask, render_template, request, escape
 from vsearch import search4letters
 
+from DBcm import UseDatabase
+
 app = Flask(__name__)
 
+app.config['dbconfig'] = { 'host': '127.0.0.1',
+				         'user': 'vsearch',
+				         'password': 'vsearchpasswd',
+				         'database': 'vsearchlogDB', }
+
 def log_request(req, res):
-    dbconfig = { 'host': '127.0.0.1',
-             'user': 'vsearch',
-             'password': 'vsearchpasswd',
-             'database': 'vsearchlogDB', }
-    import mysql.connector
-    conn = mysql.connector.connect(**dbconfig)
-    cursor = conn.cursor()
-    _SQL = """insert into log
-              (phrase, letters, ip, browser_string, results)
-              values
-              (%s, %s, %s, %s, %s)"""
-    cursor.execute(_SQL, (req.form['phrase'],
-                          req.form['letters'],
-                          req.remote_addr,
-                          req.user_agent.browser,
-                          res, ))
-    conn.commit()
-    cursor.close()
-    conn.close()
+	"""Log details of the web request and the results"""
+
+	with UseDatabase(app.config['dbconfig']) as cursor:
+	    _SQL = """insert into log
+	              (phrase, letters, ip, browser_string, results)
+	              values
+	              (%s, %s, %s, %s, %s)"""
+	    cursor.execute(_SQL, (req.form['phrase'],
+	                          req.form['letters'],
+	                          req.remote_addr,
+	                          req.user_agent.browser,
+	                          res, ))
 
 @app.route('/search4', methods=['POST'])
 def do_search():
@@ -45,13 +45,12 @@ def entry_page():
 
 @app.route('/viewlog')
 def view_the_log():
-	contents = []
-	with open('vsearch.log') as log:
-		for line in log:
-			contents.append([])
-			for item in line.split('|'):
-				contents[-1].append(escape(item))
-	titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+	with UseDatabase(app.config['dbconfig']) as cursor:
+		_SQL = """select phrase, letters, ip, browser_string, results from log"""
+		cursor.execute(_SQL)
+		contents = cursor.fetchall()
+
+	titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
 	return render_template('viewlog.html',
 							the_title = 'View Log',
 							the_row_titles = titles,
