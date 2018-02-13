53efe792c2845eb24b95e62db490545077ba68a5
Andrey Yaskov <ayaskov93@gmail.com>
2018-02-13 16:21:09 +0300

    PEP8 edits

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,39 +1,42 @@
 from flask import Flask, render_template, request, escape, session
 from vsearch import search4letters
 
-from DBcm import UseDatabase, ConnectionError, CredentialsError
+from DBcm import UseDatabase, ConnectionError, CredentialsError, SQLError
 from checker import check_logged_in
 
 app = Flask(__name__)
 
-app.config['dbconfig'] = { 'host': '127.0.0.1',
+app.config['dbconfig'] = {'host': '127.0.0.1',
 				         'user': 'vsearch',
 				         'password': 'vsearchpasswd',
 				         'database': 'vsearchlogDB', }
+
 
 @app.route('/login')
 def do_login():
 	session['logged_in'] = True
 	return 'You are now logged in'
 
+
 @app.route('/logout')
 def do_logout():
 	session.pop('logged_in')
 	return 'You are now logged out'
 
-def log_request(req, res):
-	"""Log details of the web request and the results"""
 
-	with UseDatabase(app.config['dbconfig']) as cursor:
-	    _SQL = """insert into log
-	              (phrase, letters, ip, browser_string, results)
-	              values
-	              (%s, %s, %s, %s, %s)"""
-	    cursor.execute(_SQL, (req.form['phrase'],
-	                          req.form['letters'],
-	                          req.remote_addr,
-	                          req.user_agent.browser,
-	                          res, ))
+def log_request(req, res):
+    """Log details of the web request and the results"""
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """insert into log
+                  (phrase, letters, ip, browser_string, results)
+                  values
+                  (%s, %s, %s, %s, %s)"""
+        cursor.execute(_SQL, (req.form['phrase'],
+                              req.form['letters'],
+                              req.remote_addr,
+                              req.user_agent.browser,
+                              res, ))
+
 
 @app.route('/search4', methods=['POST'])
 def do_search():
@@ -42,20 +45,22 @@ def do_search():
     title = 'Here are your results:'
     results = str(search4letters(phrase, letters))
     try:
-    	log_request(request, results)
+        log_request(request, results)
     except Exception as err:
-    	print('***** Logging failed with this error:', str(err))
+        print('***** Logging failed with this error:', str(err))
     return render_template('results.html',
                            the_phrase=phrase,
                            the_letters=letters,
                            the_title=title,
                            the_results=results,)
 
+
 @app.route('/')
 @app.route('/entry')
 def entry_page():
     return render_template('entry.html',
                            the_title='Welcome to search4letters on the web!')
+
 
 @app.route('/viewlog')
 @check_logged_in
@@ -80,7 +85,8 @@ def view_the_log():
 		print('Something went wrong:', str(err))
 	return 'Error'
 
+
 app.secret_key = 'YouWillNeverGuessMySecretKey'
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
