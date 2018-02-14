d14ef9a60d08f36285f048c13e6a34caa4de85b0
Andrey Yaskov <ayaskov93@gmail.com>
2018-02-14 18:56:52 +0300

    change css and line separators

--- Experimental/main.py
+++ Experimental/main.py
@@ -3,23 +3,23 @@ from flask import Flask, render_template, request, session
 app = Flask(__name__)
 
 
-def log_request(req, res):
-    """Log details of the web request and the results"""
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """insert into log
-                  (phrase, letters, ip, browser_string, results)
-                  values
-                  (%s, %s, %s, %s, %s)"""
-        cursor.execute(_SQL, (req.form['phrase'],
-                              req.form['letters'],
-                              req.remote_addr,
-                              req.user_agent.browser,
-                              res, ))
+# def log_request(req, res):
+#     """Log details of the web request and the results"""
+#     with UseDatabase(app.config['dbconfig']) as cursor:
+#         _SQL = """insert into log
+#                   (phrase, letters, ip, browser_string, results)
+#                   values
+#                   (%s, %s, %s, %s, %s)"""
+#         cursor.execute(_SQL, (req.form['phrase'],
+#                               req.form['letters'],
+#                               req.remote_addr,
+#                               req.user_agent.browser,
+#                               res, ))
 
 
 def analyzeSite(siteurl='https://github.com/', name='John'):
     """Return a set of the 'letters' found in 'phrase'."""
-    # return set(letters).intersection(set(phrase))
+    return siteurl + name
 
 
 @app.route('/analyze', methods=['POST'])
@@ -28,10 +28,10 @@ def do_search():
     username = request.form['username']
     title = 'Here are your results:'
     results = str(analyzeSite(siteurl, username))
-    try:
-        log_request(request, results)
-    except Exception as err:
-        print('***** Logging failed with this error:', str(err))
+    # try:
+    #     log_request(request, results)
+    # except Exception as err:
+    #     print('***** Logging failed with this error:', str(err))
     return render_template('results.html',
                            the_siteurl=siteurl,
                            the_username=username,
@@ -44,3 +44,9 @@ def do_search():
 def entry_page():
     return render_template('entry.html',
                            the_title='Welcome to search4letters on the web!')
+
+
+app.secret_key = 'YouWillNeverGuessMySecretKey'
+
+if __name__ == '__main__':
+    app.run(debug=True)
